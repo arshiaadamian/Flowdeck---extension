c6d2357e98c6e54880f9260b078da209837059b9
Arshia <arshiaadamian@gmail.com>
2026-04-11 01:20:10 -0700

    got a working version of the gemini api, tested the /parse-outline api with thunder client, and made sure the new gemini api works. Also made changes in background.js, to have it use the /parse-outline route instead to get grade weights.

--- flowdeck-server/server.js
+++ flowdeck-server/server.js
@@ -3,13 +3,14 @@ const cors = require('cors');         // Import the CORS middleware
 const { GoogleGenerativeAI } = require('@google/generative-ai'); // Import the Google Generative AI library
 require('dotenv').config(); // Load environment variables from the .env file
 
+
 // create an instance of the Express application
 const app = express();
 app.use(cors()); // Use the CORS middleware for all origins
 app.use(express.json()); // Middleware to parse JSON bodies, makes sure when a request arrives with a JSON body, it automatically gets parsed into a JavaScript Object so you can access it via req.body in your route handlers.
 const port  = 3000;
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); // Initialize the Google Generative AI client with the API key from environment variables
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });// Get the specific generative model to use for generating responses
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });// Get the specific generative model to use for generating responses
 
 // define a basic route
 app.get('/', (req, res) => {
@@ -40,12 +41,12 @@ app.post('/parse-outline', async (req, res) => {
 
         console.log("Sending prompt to AI");
         const result = await model.generateContent(prompt);
-        console.log("result is: " + result);
+        console.log("result is: " + JSON.stringify(result));
         const responseText = result.response;
-        const AItext = responseText.text();
+        const AItext = responseText.text(); // method to return the string from the response object.
         const weights = JSON.parse(AItext); // parse the response text JSON as JavaScript object
 
-        res.json({ weights }); // send the parsed weights back to the client as JSON
+        res.json({ weights: weights }); // send the parsed weights back to the client as JSON, under the "weights" array
     }
     catch (err) {
         console.error("Error parsing outline: ", err);
