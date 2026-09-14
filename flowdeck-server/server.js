01cba28633a312142e62069b2c1a5eae143f5bdd
Arshia <arshia@Arshias-MacBook-Pro.local>
2026-09-13 20:05:49 -0700

    using openAI model from Groq

--- flowdeck-server/server.js
+++ flowdeck-server/server.js
@@ -54,7 +54,7 @@ app.post('/parse-outline', async (req, res) => {
 
             // const result = await model.generateContent(prompt);
             const result = await groq.chat.completions.create({
-                model: "groq/compound",
+                model: "openai/gpt-oss-120b",
                 messages: [{role: "user", content: prompt}]
             })
 
@@ -138,7 +138,7 @@ app.post('/map-categories', async (req, res) => {
 
         // const result = await model.generateContent(prompt);
         const result = await groq.chat.completions.create({
-            model: "groq/compound",
+            model: "openai/gpt-oss-120b",
             messages: [{ role: "user", content: prompt }]
         });
         // const responseText = result.response;
